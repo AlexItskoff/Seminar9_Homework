6f6c19713999f7e8637c9f2e2e423dda3c08ef14
AlexItskoff <119017869+AlexItskoff@users.noreply.github.com>
2022-12-27 17:13:48 +0300

    Add files via upload

---
+++ Test64.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.WriteLine("Enter number N");       
+int N = int.Parse(Console.ReadLine()!);
+
+
+
+PrintNumbers(N);
+void PrintNumbers(int N)
+{
+    if (N == 1)
+    {
+        Console.Write("1");
+        return;
+    }
+        Console.Write(N +",");
+        PrintNumbers(N-1);
+        
+        
+}
---
+++ Test66.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+
+
+Console.WriteLine("Enter number M");  
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number N");  
+int N = int.Parse(Console.ReadLine());
+
+
+if (M < 0  || N < 0) 
+{
+    Console.Write("Mistake! Both numbers should be positive!");
+    return;
+}
+
+ Console.WriteLine($" Summa = {SumNumbers(M, N)}");
+
+  int SumNumbers(int M, int N)
+
+{
+     if (M == 0) return (N * (N + 1)) / 2;            
+    else if (N == 0) return (M * (M + 1)) / 2;       
+    else if (M == N) return M;                       
+    else if (M < N) return N + SumNumbers(M, N - 1); 
+    else return N + SumNumbers(M, N + 1);            
+}
